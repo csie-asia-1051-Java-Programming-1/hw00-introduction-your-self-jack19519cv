273bcb8bcfa117d15a27afa170fa8dad6c89edb6
jack19519cv <105021059@live.asia.edu.tw>
2016-09-12 15:53:02 +0800

    class01
    01

---
+++ CLASS01/src/JUNBINWANG.java
@@ -0,0 +1,15 @@
+
+public class JUNBINWANG {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("number:105021059");
+		System.out.println("name:王俊鑌 ");
+		System.out.println("class:資工數位1A ");
+
+	}
+
+}
